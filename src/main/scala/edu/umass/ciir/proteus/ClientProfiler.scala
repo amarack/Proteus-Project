8ed72fba01526fbc66563dffc34bb4a1d73d9572
Will Dabney <amarack@gmail.com>
2012-01-19 15:25:56 -0500

    fixes

--- src/main/scala/edu/umass/ciir/proteus/ClientProfiler.scala
+++ src/main/scala/edu/umass/ciir/proteus/ClientProfiler.scala
@@ -6,17 +6,22 @@ import akka.dispatch.Future
 import scala.collection.JavaConverters._
 import edu.umass.ciir.proteus.protocol.ProteusProtocol._
 
+
 object clientProfilerApp extends App {
-  val client = new LibrarianClient(args(0), args(1).toInt)
+  val total_start = System.currentTimeMillis
   
+  val client = new LibrarianClient(args(0), args(1).toInt)
+  var total_send = 0.0
   // Run some queries...
   for (i <- 0 until 1000) {
+    
+    val send_start = System.currentTimeMillis
     // Do a query
     val results = client.query("Some random text " + i.toString, 
         client.convertTypes(List("collection", "page", "person", "location", "picture"))).get
     
     // Look up some of the results
-    val parallel_requests = results.getResultsList.asScala.map(result => {
+    val parallel_requests = results.getResultsList.asScala.slice(0,10).map(result => {
       val lookup = result.getProteusType match {
         case ProteusType.COLLECTION => client.lookupCollection(result)
         case ProteusType.PAGE => client.lookupPage(result)
@@ -34,7 +39,11 @@ object clientProfilerApp extends App {
         case _ => Future {None}
       }
       (lookup, contents)})
-      
+    val send_stop = System.currentTimeMillis  
+    total_send += (send_stop - send_start)
     parallel_requests.map(r => (r._1.get, r._2.get))
   }
+  val total_stop = System.currentTimeMillis
+  System.out.println("Start: " + total_start + " Stop: " + total_stop + " Total Time: " + (total_stop - total_start))
+  System.out.println("Total time spend sending: " + total_send)
 }
